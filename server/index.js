df984ae97e4c9f55419d8fd6e23f30e444ddee48
Sunki Baek <sunki.baek@gmail.com>
2017-11-22 20:40:52 +0900

    add key to mock data

--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@ const http = require('http');
 const port = 3000;
 
 const chatRooms = [{
+    key: '1',
     name: "Ana Jacobson",
     time: "5 minutes"
 },{
+    key: '2',
     name: "Michel Shelton",
     time: "3 minutes"
 },{
+    key: '3',
     name: "Todd Fisher",
     time: "5 minutes"
 }];
